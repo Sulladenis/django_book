3a60fa8f516fb927a55cb78c95f8ad4ee7abae9f
sulla <sulladenis@mail.com>
2019-06-05 16:58:20 +1000

    completed to page 42

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from .models import Bb
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список обьявдений.")
+    s = 'Список обьявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n' + str(bb.price) + '\r\n\r\n\r\n'
+    return HttpResponse(s, content_type='text/plan; charset=utf-8')
