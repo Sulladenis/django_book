616d253251f02da453b876df7d63f358868e7760
sulla <sulladenis@mail.com>
2019-06-06 15:39:31 +1000

    Completed assignments to Chapter 2.3 57

--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Bb
 
 def index(request):
-    s = 'Список обьявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n' + str(bb.price) + '\r\n\r\n\r\n'
-    return HttpResponse(s, content_type='text/plan; charset=utf-8')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
