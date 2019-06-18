66458b7b005e58e2a3e159f29323ec294cb77534
Sulldenis <sulladenis@gmail.com>
2019-06-18 17:25:23 +1000

    page 114

--- bboard/models.py
+++ bboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Товар')
@@ -7,10 +8,21 @@ class Bb(models.Model):
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликованно')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
+
+    def title_and_price(self):
+        if self.price:
+            return '%s (%.2f)' % (self.title, self.price)
+        else:
+            return self.title
+
+    title_and_price.short_description = 'Название и цена'
+
     class Meta:
         verbose_name_plural = 'Обьявления'
         verbose_name = 'Обьявление'
         ordering = ['-published']
+        unique_together = ('title', 'published')
+        get_latest_by = ('published')
 
 class Rubric(models.Model):
     name = models.CharField(max_length=15, verbose_name='Название')
@@ -22,3 +34,14 @@ class Rubric(models.Model):
         verbose_name = 'Рубрика'
         verbose_name_plural = 'Рубрики'
         ordering = ['name']
+
+class AdvUser(User):
+    is_activated = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, parent_link=True)
+
+class Spare(models.Model):
+    name = models.CharField(max_length=30)
+
+class Machine(models.Model):
+    name = models.CharField(max_length=30)
+    spares = models.ManyToManyField(Spare)
